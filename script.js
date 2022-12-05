972adeb6771a7e3037858f252eee81251ff4fd99
ajit1266 <100941072+ajit1266@users.noreply.github.com>
2022-12-05 16:23:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //your code here
 
 document.querySelector('#swap').addEventListener('click', function(){
+document.querySelector('#swap').classList.remove("button_day");
+document.querySelector('#app').classList.remove("day");
 document.querySelector('#swap').classList.add("button_night");
 document.querySelector('#app').classList.add("night");
 })
